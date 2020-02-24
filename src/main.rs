e4d6c8b3ece37c248c138ad3b65d46cef829a54f
Nemin32 <pergerlori@gmail.com>
2020-02-24 15:17:00 +0100

    Baseline working status.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate lazy_static;
 
 use native_tls::TlsConnector;
+use std::collections::HashSet;
+use std::hash::Hash;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
@@ -10,7 +12,12 @@ use serde::{Deserialize, Serialize};
 struct Thread {
     poster: String,
     date: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     title: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    subforum: Option<String>,
+
     up_votes: usize,
     down_votes: usize,
     body: String,
@@ -44,114 +51,116 @@ fn make_request(request: String) -> String {
     split[1].clone()
 }
 
-fn sync_threads<T>(threads: &mut Vec<std::thread::JoinHandle<Vec<T>>>, results: &mut Vec<T>) {
-    println!("Catching up...");
+fn sync_threads<T>(threads: &mut Vec<std::thread::JoinHandle<HashSet<T>>>, results: &mut HashSet<T>)
+    where
+    T: Eq + Hash,
+{
+    //println!("Catching up...");
     for t in threads.drain(..) {
-	let arr = t.join().unwrap();
-	results.extend(arr.into_iter());
+        let arr = t.join().unwrap();
+
+        for elem in arr {
+            results.insert(elem);
+        }
     }
 }
 
-fn top_poszt_ids(n: usize) -> Vec<(String, String)> {
+fn top_poszt_ids(n: usize) -> HashSet<(String, String)> {
     let mut threads = vec![];
-    let mut results = vec![];
+    let mut results = HashSet::new();
 
     for i in 0..n {
-	threads.push(std::thread::spawn(move || {
-	    let url = format!("/api/q98U6Ykw/discussions?sort_type=best&num_loaded={}", i*50);
-	    println!("{}", url);
-	    let response = make_request(url);
-	    let json: serde_json::Value = serde_json::from_str(&response).unwrap();
+        threads.push(std::thread::spawn(move || {
+            let url = format!(
+                "/api/q98U6Ykw/discussions?sort_type=best&num_loaded={}",
+                i * 50
+                );
+            println!("{}", url);
+            let response = make_request(url);
+            let json: serde_json::Value = serde_json::from_str(&response).unwrap();
 
-	    collect_ids(json)
-	}));
+            collect_ids(json)
+        }));
 
-	if threads.len() > *CPU_COUNT {
-	    sync_threads(&mut threads, &mut results);
-	}
+        if threads.len() > *CPU_COUNT {
+            sync_threads(&mut threads, &mut results);
+        }
     }
 
     sync_threads(&mut threads, &mut results);
 
     println!("{}", results.len());
-    
+
     results
 }
 
-fn collect_ids(json: serde_json::Value) -> Vec<(String, String)> {
+fn collect_ids(json: serde_json::Value) -> HashSet<(String, String)> {
     use regex::Regex;
     lazy_static! {
         static ref REG: Regex =
             Regex::new(r#"data-application-id="(.*?)" data-discussion-id="(.*?)""#).unwrap();
     }
 
-    let mut results: Vec<(String, String)> = vec![];
+    let mut results: HashSet<(String, String)> = HashSet::new();
 
     if let Some(json_results) = json["results"].as_str() {
-	for capture in REG.captures_iter(&json_results) {
-	    let tuple: (String, String) = (String::from(&capture[1]), String::from(&capture[2]));
-	    if !results
-		.iter()
-		.any(|elem| elem.0 == tuple.0 && elem.1 == tuple.1)
-	    {
-		results.push(tuple);
-	    }
-	}
+        for capture in REG.captures_iter(&json_results) {
+            let tuple: (String, String) = (String::from(&capture[1]), String::from(&capture[2]));
+            results.insert(tuple);
+        }
     }
 
     if let Some(json_results) = json["discussions"].as_str() {
-	for capture in REG.captures_iter(&json_results) {
-	    let tuple: (String, String) = (String::from(&capture[1]), String::from(&capture[2]));
-	    if !results
-		.iter()
-		.any(|elem| elem.0 == tuple.0 && elem.1 == tuple.1)
-	    {
-		results.push(tuple);
-	    }
-	}
+        for capture in REG.captures_iter(&json_results) {
+            let tuple: (String, String) = (String::from(&capture[1]), String::from(&capture[2]));
+            results.insert(tuple);
+        }
     }
 
     results
 }
 
-fn get_user_thread_ids(nev: &String) -> Vec<(String, String)> {
+fn get_user_thread_ids(nev: &String) -> HashSet<(String, String)> {
     let initial_response = make_request(format!("/hu/player/EUNE/{}?json_wrap=1", nev));
-    let json: serde_json::Value = serde_json::from_str(&initial_response).unwrap();
-    let count = json["searchResultsCount"].as_i64().unwrap();
 
-    let mut results: Vec<(String, String)> = collect_ids(json);
+    if let Ok(json) = serde_json::from_str(&initial_response) {
+        let json: serde_json::Value = json;
+        let count = json["searchResultsCount"].as_i64().unwrap();
 
-    let mut threads = Vec::new();
+        let mut results: HashSet<(String, String)> = collect_ids(json);
+        let mut threads = Vec::new();
 
-    if count > 50 {
-        let rounds = (count / 50) + 1;
+        if count > 50 {
+            let rounds = (count / 50) + 1;
 
-        for round in 1..rounds {
-            let nev = nev.clone();
-            threads.push(std::thread::spawn(move || {
-                let url = format!(
-                    "/hu/player/EUNE/{}?json_wrap=1&num_loaded={}",
-                    nev,
-                    50 + round * 50
-                );
-                println!("{}", url);
+            for round in 1..rounds {
+                let nev = nev.clone();
+                threads.push(std::thread::spawn(move || {
+                    let url = format!(
+                        "/hu/player/EUNE/{}?json_wrap=1&num_loaded={}",
+                        nev,
+                        50 + round * 50
+                        );
+                    //println!("Page {}/{}", round, rounds);
 
-                let response = make_request(url);
-                let json: serde_json::Value = serde_json::from_str(&response).unwrap();
+                    let response = make_request(url);
+                    let json: serde_json::Value = serde_json::from_str(&response).unwrap();
 
-                collect_ids(json)
-            }));
+                    collect_ids(json)
+                }));
 
-            if threads.len() >= *CPU_COUNT {
-		sync_threads(&mut threads, &mut results);
+                if threads.len() >= *CPU_COUNT {
+                    sync_threads(&mut threads, &mut results);
+                }
             }
         }
+
+        sync_threads(&mut threads, &mut results);
+        results
+    } else {
+        println!("ERROR! Couldn't process {}.", nev);
+        HashSet::new()
     }
-
-
-    sync_threads(&mut threads, &mut results);
-    println!("Finished collecting IDs.");
-    results
 }
 
 fn download_post(app_id: &String, disc_id: &String) -> serde_json::Value {
@@ -159,88 +168,126 @@ fn download_post(app_id: &String, disc_id: &String) -> serde_json::Value {
     serde_json::from_str(&make_request(url)).unwrap()
 }
 
-fn print_thread(root: &serde_json::Value, depth: usize) -> Thread {
+fn process_thread(root: &serde_json::Value, head: bool, names: &mut HashSet<String>) -> Thread {
     use std::convert::TryInto;
 
-    let mut head = Thread {
+    let mut thread = Thread {
         poster: String::from(
-            root["user"]["name"]
-                .as_str()
-                .unwrap_or("[NEM SIKERÜLT KIOLVASNI]"),
-        ),
-        date: String::from(
-            root["createdAt"]
-                .as_str()
-                .unwrap_or("[NEM SIKERÜLT KIOLVASNI]"),
-        ),
-        up_votes: root["upVotes"].as_u64().unwrap().try_into().unwrap(),
-        down_votes: root["downVotes"].as_u64().unwrap().try_into().unwrap(),
-        replies: Vec::new(),
-        body: {
-            if depth == 0 {
-                String::from(root["content"]["body"].as_str().unwrap_or("[ÜRES]"))
-            } else {
-                String::from(root["message"].as_str().unwrap_or("[ÜRES]"))
-            }
-        },
-        title: {
-            if depth == 0 {
-                Some(String::from(
-                    root["title"]
+                    root["user"]["name"]
+                    .as_str()
+                    .unwrap_or("[NEM SIKERÜLT KIOLVASNI]"),
+                    ),
+                    date: String::from(
+                        root["createdAt"]
                         .as_str()
-                        .unwrap_or("[NEM SIKERÜLT KIOLVVASNI]"),
-                ))
-            } else {
-                None
-            }
-        },
+                        .unwrap_or("[NEM SIKERÜLT KIOLVASNI]"),
+                        ),
+                        up_votes: root["upVotes"].as_u64().unwrap().try_into().unwrap_or(0),
+                        down_votes: root["downVotes"].as_u64().unwrap().try_into().unwrap_or(0),
+                        replies: Vec::new(),
+                        body: {
+                            if head {
+                                String::from(root["content"]["body"].as_str().unwrap_or("[ÜRES]"))
+                            } else {
+                                String::from(root["message"].as_str().unwrap_or("[ÜRES]"))
+                            }
+                        },
+                        title: {
+                            if head {
+                                Some(String::from(
+                                        root["title"]
+                                        .as_str()
+                                        .unwrap_or("[NEM SIKERÜLT KIOLVVASNI]")))
+                            } else {
+                                None
+                            }
+                        },
+                        subforum: {
+                            if head {
+                                Some(String::from(
+                                        root["application"]["name"]
+                                        .as_str()
+                                        .unwrap_or("[NEM SIKERÜLT KIOLVVASNI]")))
+                            } else {
+                                None
+                            }
+                        }
     };
 
-    if depth == 0 {
+    if let Some(name) = root["user"]["name"].as_str() {
+        names.insert(name.to_string());
+    }
+
+    if head {
         if let Some(comments) = root["comments"]["comments"].as_array() {
             for msg in comments {
-                head.replies.push(print_thread(msg, depth + 2));
+                thread.replies.push(process_thread(msg, false, names));
             }
         }
     } else {
         if let Some(comments) = root["replies"]["comments"].as_array() {
             for msg in comments {
-                head.replies.push(print_thread(msg, depth + 2));
+                thread.replies.push(process_thread(msg, false, names));
             }
         }
     }
 
-    head
+    thread
 }
 
-fn download_and_write_ids(folder_name: &String, ids: Vec<(String, String)>) {
+fn write_file(thread: &Thread, nums: &mut std::collections::HashMap<String, usize>) {
     let dir = "../posztok";
     let _ = std::fs::create_dir(dir);
-    let _ = std::fs::create_dir(format!("{}/{}", dir, folder_name));
+    let _ = std::fs::create_dir(format!("{}/{}", dir, thread.poster));
 
+    let mut file = {
+        if let Some(i) = nums.remove(&thread.poster) {
+            let file =
+                std::fs::File::create(&format!("{}/{}/{}.json", dir, thread.poster, i)).unwrap();
+            nums.insert(thread.poster.clone(), i + 1);
+            file
+        } else {
+            let file =
+                std::fs::File::create(&format!("{}/{}/{}.json", dir, thread.poster, 0)).unwrap();
+            nums.insert(thread.poster.clone(), 1);
+            file
+        }
+    };
+
+    serde_json::to_writer(&mut file, &thread).unwrap();
+}
+
+fn download_ids(ids: &HashSet<(String, String)>) -> (Vec<Thread>, HashSet<String>) {
     let count = ids.len();
     let mut done = 0;
 
     let mut threads = Vec::new();
+    let mut results = Vec::new();
+    let mut names = HashSet::new();
 
-    for (i, (app_id, disc_id)) in ids.iter().enumerate() {
+    for (app_id, disc_id) in ids {
         let app_id = app_id.clone();
         let disc_id = disc_id.clone();
-        let folder_name = folder_name.clone();
 
         threads.push(std::thread::spawn(move || {
-            let poszt = download_post(&app_id, &disc_id);
+            let mut names = HashSet::new();
 
-            let mut file = std::fs::File::create(&format!("{}/{}/{}.json", dir, folder_name, i)).unwrap();
-            let thread = print_thread(&poszt["discussion"], 0);
-
-            serde_json::to_writer(&mut file, &thread).unwrap();
+            (process_thread(&download_post(&app_id, &disc_id)["discussion"], true, &mut names), names)
         }));
 
         if threads.len() >= *CPU_COUNT {
             println!("Catching up...");
             for t in threads.drain(..) {
-                t.join().unwrap();
+
+                if let Ok(retval) = t.join() {
+                    results.push(retval.0);
+
+                    for elem in retval.1 {
+                        names.insert(elem);
+                    }
+                } else {
+                    println!("Eror unwrapping thread.");
+                }
                 done += 1;
                 println!("Done: {}/{}", done, count);
             }
@@ -248,12 +295,23 @@ fn download_and_write_ids(folder_name: &String, ids: Vec<(String, String)>) {
     }
 
     for t in threads {
-        t.join().unwrap();
+
+        if let Ok(retval) = t.join() {
+            results.push(retval.0);
+
+            for elem in retval.1 {
+                names.insert(elem);
+            }
+        } else {
+            println!("Eror unwrapping thread.");
+        }
+
         done += 1;
         println!("Done: {}/{}", done, count);
     }
-}
 
+    (results, names)
+}
 
 fn main() {
     println!("CHARON vagyok, az alvilág hajósa.\nVálaszd ki mit akarsz tenni:\n\n1 - Top posztok lementése.\n2 - Egy felhasználó posztjainak lementése.\n3 - Egy specifikus poszt letöltése.");
@@ -268,8 +326,45 @@ fn main() {
         input.pop();
     }
 
-    let ids = top_poszt_ids(20);//get_user_thread_ids(&input);
+    let root = get_user_thread_ids(&input);
+    //let ids = top_poszt_ids(20);
+    let posts = download_ids(&root);
 
-    //download_and_write_ids(&input, ids);
-    download_and_write_ids(&String::from("Top1000"), ids);
+    let mut name_file = std::fs::File::create("names.txt").unwrap();
+    let mut id_file = std::fs::File::create("ids.txt").unwrap();
+
+    let mut extra_ids = HashSet::new();
+
+    let length = posts.1.len();
+
+    for (i, name) in posts.1.iter().enumerate() {
+        println!("[{}/{}] Collecting {}'s IDs.", i, length, name);
+        let ids = get_user_thread_ids(&name);
+
+        for id in &ids {
+            if let None = root.get(&id) {
+                if !extra_ids.insert(id.clone()) {
+                    if let Err(e) = write!(id_file, "({}, {})\n", id.0, id.1) {
+                        println!("Error: {}", e);
+                    }
+                }
+            }
+        }
+
+        if let Err(e) = write!(name_file, "{}\n", name) {
+            println!("Error: {}", e);
+        }
+    }
+
+    println!("---\nI have {} IDs.\n---", extra_ids.len());
+
+    let mut extra_threads = download_ids(&extra_ids);
+
+    extra_threads.0.extend(posts.0);
+
+    let mut nums = std::collections::HashMap::new();
+    extra_threads.0.sort_by(|a, b| b.up_votes.cmp(&a.up_votes));
+    for post in extra_threads.0.iter() {
+        write_file(post, &mut nums);
+    }
 }
