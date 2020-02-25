3b90ee1296c22f00179e49281f4c3bd044edea55
Nemin32 <pergerlori@gmail.com>
2020-02-25 22:54:17 +0100

    Revert "Unfinished rework."
    This reverts commit 9ea8e69a59866e17be4059bb1ab9f3dde1d56f3f.

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,9 @@ use std::hash::Hash;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
+use std::sync::{RwLock, Arc};
+
 use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize)]
-struct Link {
-    description: Option<String>,
-    url: Option<String>,
-    image: Option<String>
-}
-
 #[derive(Deserialize, Serialize)]
 struct Thread {
     poster: String,
@@ -26,31 +20,25 @@ struct Thread {
     title: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     subforum: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    embed: Option<Link>,
 
     up_votes: usize,
     down_votes: usize,
     body: String,
-    replies: Vec<Thread>
+    replies: Vec<Thread>,
 }
 
 lazy_static! {
     static ref CPU_COUNT: usize = num_cpus::get();
 }
 
-fn make_connection() -> native_tls::TlsStream<TcpStream> {
-    let connector = TlsConnector::new().unwrap();
-    let _stream = TcpStream::connect("boards.eune.leagueoflegends.com:443").unwrap();
-    let mut _stream = connector
-        .connect("boards.eune.leagueoflegends.com", _stream)
-        .unwrap();
-
-    _stream
-}
-
-fn make_request(stream: &mut native_tls::TlsStream<TcpStream>, request: String) -> String {
+fn make_request(request: String) -> String {
     let url = format!("GET {} HTTP/1.0\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: boards.eune.leagueoflegends.com\r\n\r\n", request);
+
+    let connector = TlsConnector::new().unwrap();
+    let stream = TcpStream::connect("boards.eune.leagueoflegends.com:443").unwrap();
+    let mut stream = connector
+        .connect("boards.eune.leagueoflegends.com", stream)
+        .unwrap();
 
     stream.write(url.as_bytes()).unwrap();
     let mut resp = vec![];
@@ -80,6 +68,35 @@ fn sync_threads<T>(threads: &mut Vec<std::thread::JoinHandle<HashSet<T>>>, resul
     }
 }
 
+fn top_poszt_ids(n: usize) -> HashSet<(String, String)> {
+    let mut threads = vec![];
+    let mut results = HashSet::new();
+
+    for i in 0..n {
+        threads.push(std::thread::spawn(move || {
+            let url = format!(
+                "/api/q98U6Ykw/discussions?sort_type=best&num_loaded={}",
+                i * 50
+                );
+            println!("{}", url);
+            let response = make_request(url);
+            let json: serde_json::Value = serde_json::from_str(&response).unwrap();
+
+            collect_ids(json)
+        }));
+
+        if threads.len() > *CPU_COUNT {
+            sync_threads(&mut threads, &mut results);
+        }
+    }
+
+    sync_threads(&mut threads, &mut results);
+
+    println!("{}", results.len());
+
+    results
+}
+
 fn collect_ids(json: serde_json::Value) -> HashSet<(String, String)> {
     use regex::Regex;
     lazy_static! {
@@ -107,8 +124,7 @@ fn collect_ids(json: serde_json::Value) -> HashSet<(String, String)> {
 }
 
 fn get_user_ids(nev: &String) -> HashSet<(String, String)> {
-    let mut conn = make_connection();
-    let initial_response = make_request(conn.try_clone(), format!("/hu/player/EUNE/{}?json_wrap=1", nev));
+    let initial_response = make_request(format!("/hu/player/EUNE/{}?json_wrap=1", nev));
 
     if let Ok(json) = serde_json::from_str(&initial_response) {
         let json: serde_json::Value = json;
@@ -145,7 +161,7 @@ fn get_user_ids(nev: &String) -> HashSet<(String, String)> {
         sync_threads(&mut threads, &mut results);
         results
     } else {
-        print!(" ERROR! Couldn't process {}. ", nev);
+        println!("ERROR! Couldn't process {}.", nev);
         HashSet::new()
     }
 }
@@ -195,41 +211,6 @@ fn process_raw_thread(root: &serde_json::Value, head: bool, names: &mut HashSet<
                                         root["application"]["name"]
                                         .as_str()
                                         .unwrap_or("[NEM SIKERÜLT KIOLVVASNI]")))
-                            } else {
-                                None
-                            }
-                        },
-                        embed: {
-                            if let Some(link_root) = root["content"].get("sharedLink") {
-                                if let Some(shared_link) = link_root.as_object() {
-                                    let description = {
-                                        if let Some(node) = shared_link.get("description") {
-                                            Some(String::from(node.as_str().unwrap_or("[NINCS LEÍRÁS]")))
-                                        } else {
-                                            None
-                                        }
-                                    };
-
-                                    let url = {
-                                        if let Some(node) = shared_link.get("url") {
-                                            Some(String::from(node.as_str().unwrap_or("[ROSSZ LINK]")))
-                                        } else {
-                                            None
-                                        }
-                                    };
-
-                                    let image = {
-                                        if let Some(node) = shared_link.get("image") {
-                                            Some(String::from(node.as_str().unwrap_or("[NINCS KÉP]")))
-                                        } else {
-                                            None
-                                        }
-                                    };
-
-                                    Some(Link { description, url, image })
-                                } else {
-                                    None
-                                }
                             } else {
                                 None
                             }
@@ -283,23 +264,15 @@ fn process_threads(ids: &HashSet<(String, String)>) -> (Vec<Thread>, HashSet<Str
     let count = ids.len();
     let mut done = 0;
 
-    if count > 0 {
-        println!("[{} new threads]", count);
-    } else {
-        println!("[No new threads]");
-    }
-
+    println!("[{} threads]", count);
+    
     let mut threads = Vec::new();
     let mut results = Vec::new();
     let mut names = HashSet::new();
 
-    let mut conn = make_connection();
-
     for (app_id, disc_id) in ids {
         let app_id = app_id.clone();
         let disc_id = disc_id.clone();
-
-        let mut conn = conn.copy();
 
         threads.push(std::thread::spawn(move || {
             let mut names = HashSet::new();
@@ -310,9 +283,13 @@ fn process_threads(ids: &HashSet<(String, String)>) -> (Vec<Thread>, HashSet<Str
         if threads.len() >= *CPU_COUNT {
             //println!("Catching up...");
             for t in threads.drain(..) {
+
                 if let Ok(retval) = t.join() {
                     results.push(retval.0);
-                    names.extend(retval.1);
+
+                    for elem in retval.1 {
+                        names.insert(elem);
+                    }
                 } else {
                     println!("Eror unwrapping thread.");
                 }
@@ -329,7 +306,10 @@ fn process_threads(ids: &HashSet<(String, String)>) -> (Vec<Thread>, HashSet<Str
 
         if let Ok(retval) = t.join() {
             results.push(retval.0);
-            names.extend(retval.1);
+
+            for elem in retval.1 {
+                names.insert(elem);
+            }
         } else {
             println!("Eror unwrapping thread.");
         }
@@ -343,69 +323,78 @@ fn process_threads(ids: &HashSet<(String, String)>) -> (Vec<Thread>, HashSet<Str
     (results, names)
 }
 
-fn add_names(names: HashSet<String>, name: &String, name_queue: &mut HashMap<String, bool>) {
-    name_queue.insert(name.to_string(), true);
+fn add_names(names: HashSet<String>, name: &String, name_queue: Arc<RwLock<HashMap<String, bool>>>) {
+    if let Ok(mut lock) = name_queue.write() {
+        lock.insert(name.to_string(), true);
 
-    let mut names = names.clone();
-    names.retain(|elem| {name_queue.contains_key(elem) == false});
+        let mut names = names.clone();
+        names.retain(|elem| {lock.contains_key(elem) == false});
 
-    for name in names {
-	name_queue.insert(name, false);
+        for name in names {
+            lock.insert(name, false);
+        }
+    } else {
+        println!("Couldn't lock RwLock.");
     }
 }
 
-fn prune_ids(ids: &mut HashSet<(String, String)>, processed_ids: &mut HashSet<(String, String)>) {
-    ids.retain(|elem| processed_ids.insert(elem.clone()));
+fn prune_ids(ids: &mut HashSet<(String, String)>, processed_ids: Arc<RwLock<HashSet<(String, String)>>>) {
+    if let Ok(mut lock) = processed_ids.write() {
+	ids.retain(|elem| lock.insert(elem.clone()));
+    }
 }
 
-fn process_player(name: &String, name_queue: &mut HashMap<String, bool>, processed_ids: &mut HashSet<(String, String)>) -> Vec<Thread> {
-    let mut conn = make_connection();
-
-    let mut ids = get_user_ids(&conn, name);
+fn process_player(name: &String, name_queue: Arc<RwLock<HashMap<String, bool>>>, processed_ids: Arc<RwLock<HashSet<(String, String)>>>) -> Vec<Thread> {
+    let mut ids = get_user_ids(name);
     prune_ids(&mut ids, processed_ids);
-    let (threads, names) = process_threads(&conn, &ids);
+    let (threads, names) = process_threads(&ids);
 
     add_names(names, name, name_queue);
 
     threads
 }
 
+
+
 fn main() {
     println!("Ez itt STYX, a fórum lementő program.");
 
-    let mut names: HashMap<String, bool> = HashMap::new();
-    let mut ids: HashSet<(String, String)> = HashSet::new();
-    names.insert(String::from("Nemin"), false);
-
-    //names.insert(String::from("Support LeBlanc"), false);
+    let mut name_hash: HashMap<String, bool> = HashMap::new();
+    let id_hash: HashSet<(String, String)> = HashSet::new();
+    name_hash.insert(String::from("Nemin"), false);
     /*name_hash.insert(String::from("Zooty"), true);
     name_hash.insert(String::from("Mind The Gap"), true);
     name_hash.insert(String::from("Shikaichi"), true);
     name_hash.insert(String::from("Gamma Ray"), false);*/
     //name_hash.insert(String::from("AsD The Dreamer"), false);
 
+    let names = Arc::new(RwLock::new(name_hash));
+    let ids = Arc::new(RwLock::new(id_hash));
+
     //process_player(&String::from("Gamma Ray"), names.clone());
 
 
     let mut nums = std::collections::HashMap::new();
-    let mut thread_count = 0;
 
+    let mut thread_count = 0;
+    
     loop {
-	if names.iter().all(|(_, val)| {*val}) {break;}
+        if names.read().unwrap().iter().all(|(_, val)| {*val}) {break;}
 
         let (done, all) = {
             let mut done = 0;
+            let lock = names.read().unwrap();
 
-            for (_, val) in names.iter() {
+            for (_, val) in lock.iter() {
                 if *val {done+=1;}
             }
 
-            (done, names.len())
+            (done, lock.len())
         };
 
         let name: Option<String> = {
             let mut retval = None;
-            for (candidate, processed) in names.iter() {
+            for (candidate, processed) in names.read().unwrap().iter() {
                 if !processed {
                     retval = Some(candidate.clone());
                     break;
@@ -416,17 +405,73 @@ fn main() {
 
 
         if let Some(name) = name {
-            print!("[{}/{} ({}%) ({})] {} ", done, all, (((done as f64)/(all as f64))*100.0) as usize, thread_count, name);
-            let threads = process_player(&name, &mut names, &mut ids);
+            let names = names.clone();
+	    let ids = ids.clone();
+            print!("[{}/{} ({}%)] {} ", done, all, (((done as f64)/(all as f64))*100.0) as usize, name);
+            let threads = process_player(&name, names, ids);
 
-            if threads.len() > 0 {
-                for post in threads.iter() {
-                    write_file(post, &mut nums);
-                    thread_count += 1;
-                }
-            }
+            for post in threads.iter() {
+                write_file(post, &mut nums);
+
+		thread_count += 1;
+		if thread_count % 1000 == 0 {
+		    println!("Currently written out over {} threads.", thread_count);
+		}
+	    }
         }
     }
 
     println!("Final thread-count: {} threads.", thread_count);
+
+    /*let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Bad input!");
+
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+
+    let root = get_user_thread_ids(&input);
+    //let ids = top_poszt_ids(20);
+    let posts = download_ids(&root);
+
+    let mut name_file = std::fs::File::create("names.txt").unwrap();
+    let mut id_file = std::fs::File::create("ids.txt").unwrap();
+
+    let mut extra_ids = HashSet::new();
+
+    let length = posts.1.len();
+
+    for (i, name) in posts.1.iter().enumerate() {
+        println!("[{}/{}] Collecting {}'s IDs.", i, length, name);
+        let ids = get_user_thread_ids(&name);
+
+        for id in &ids {
+            if let None = root.get(&id) {
+                if !extra_ids.insert(id.clone()) {
+                    if let Err(e) = write!(id_file, "({}, {})\n", id.0, id.1) {
+                        println!("Error: {}", e);
+                    }
+                }
+            }
+        }
+
+        if let Err(e) = write!(name_file, "{}\n", name) {
+            println!("Error: {}", e);
+        }
+    }
+
+    println!("---\nI have {} IDs.\n---", extra_ids.len());
+
+    let mut extra_threads = download_ids(&extra_ids);
+
+    extra_threads.0.extend(posts.0);
+
+    let mut nums = std::collections::HashMap::new();
+    extra_threads.0.sort_by(|a, b| b.up_votes.cmp(&a.up_votes));
+    for post in extra_threads.0.iter() {
+        write_file(post, &mut nums);
+    }*/
 }
